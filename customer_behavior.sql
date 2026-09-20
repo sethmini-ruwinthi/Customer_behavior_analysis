06dc2cc56792ba112ce84a4fe7a34575edc9d25d
Sethmini Herath <sethminiruwinthi311@gmail.com>
2026-09-20 21:12:56 +0530

    Add files via upload

---
+++ customer_behavior.sql
@@ -0,0 +1,21 @@
+SELECT gender, SUM(purchase_amount) AS revenue FROM customer GROUP BY gender;
+
+SELECT customer_id, purchase_amount FROM customer WHERE discount_applied = 'Yes' AND purchase_amount >= (SELECT AVG(purchase_amount) FROM customer);
+
+SELECT item_purchased, ROUND(CAST(AVG(review_rating) AS numeric),2) AS Average_review_rating FROM customer GROUP BY item_purchased ORDER BY AVG(review_rating) DESC LIMIT 5;
+
+SELECT shipping_type, ROUND(AVG(purchase_amount), 2) FROM customer WHERE shipping_type IN ('Standard', 'Express') GROUP BY shipping_type;
+
+SELECT subscription_status, COUNT(customer_id) AS total_customers, ROUND(AVG(purchase_amount), 2) AS avg_spend, ROUND(SUM(purchase_amount)) AS total_revenue FROM customer GROUP BY subscription_status ORDER BY total_revenue, avg_spend DESC;
+
+SELECT item_purchased, ROUND(SUM(100 * CASE WHEN discount_applied = 'Yes' THEN 1 ELSE 0 END)/COUNT(*),2) AS discount_rate FROM customer GROUP BY item_purchased ORDER BY discount_rate DESC LIMIT 5;
+
+WITH customer_type AS (SELECT customer_id, previous_purchases, CASE WHEN previous_purchases=1 THEN 'New' WHEN previous_purchases BETWEEN 2 AND 10 THEN 'Returning' ELSE 'Loyal' END AS customer_segment FROM customer)
+SELECT customer_segment, COUNT(*) AS Number_of_customers FROM customer_type GROUP BY customer_segment;
+
+WITH item_counts AS ( SELECT category, item_purchased, COUNT(customer_id) AS total_orders, ROW_NUMBER() OVER (PARTITION BY category ORDER BY COUNT(customer_id) DESC) AS item_rank FROM customer GROUP BY category, item_purchased)
+SELECT item_rank, category, item_purchased, total_orders FROM item_counts WHERE item_rank <=3;
+
+SELECT subscription_status, COUNT(customer_id) AS repeat_buyers FROM customer WHERE previous_purchases >5 GROUP BY subscription_status ;
+
+SELECT age_group, SUM(purchase_amount)AS total_revenue FROM customer GROUP BY age_group ORDER BY total_revenue DESC;
